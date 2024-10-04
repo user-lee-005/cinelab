382a89162daccd9633d5910383c78b6688bfb829
user-lee-005 <93381862+user-lee-005@users.noreply.github.com>
2024-10-04 19:14:53 +0530

    Added Loader and Clear button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
